6247bbc0cb86046da25c1500a96518f3f617c93b
Dean Jackson <deanishe@deanishe.net>
2016-11-01 16:02:23 +0100

    Enable Quick Look preview of subreddits/comments/posts #4
    - Allow opening both article and Reddit comments at the same time

--- README.md
+++ README.md
@@ -12,40 +12,46 @@ Download
 
 Get Reddit for Alfred from [GitHub][gh-releases] or [Packal][packal-page].
 
-**Note**: Versions 1.4 and later are *not* compatible with Alfred 2. Please download [v1.3][v1.3] if you're using Alfred 2.
+**Note**: Version 1.4 (and above) is *not* compatible with Alfred 2. Please download [v1.3][v13] if you're using Alfred 2.
 
 
 Usage
 -----
 
 - `r/` — Show list of popular subreddits
+	- `⇥`, `↩` or `⌘+NUM` — Show 50 hottest posts in selected subreddit
+	- `⌘+↩` — Open subreddit in browser
+	- `⇧` or `⌘+Y` — Show Quick Look preview of subreddit
 - `r/<query>` — Search for subreddits matching `<query>`
     - `⇥` — Show 50 hottest posts in selected subreddit (same as appending `/`)
     - `↩` or `⌘+NUM` — Open subreddit in browser
-- `r/<subreddit>/` — Show 50 hottest posts on subreddit `<subreddit>`
-    - `⇧` or `⌘+Y` — Show quicklook preview of comments page
-- `r/<subreddit>/<query>` — Filter posts by `<query>`
-    - `↩` or `⌘+NUM` — Open post URL in default browser
+    - `⇧` or `⌘+Y` — Show Quick Look preview of subreddit
+- `r/<subreddit>/[<query>]` — Show 50 hottest posts on subreddit `<subreddit>`, filtered by `<query>` if present
+    - `↩` or `⌘+NUM` — Open article in default browser
     - `⌘+↩` — Open Reddit comments in default browser
-    - `⌘+L` — Show full post title in Alfred's Large Text window
+    - `⌥+↩` — Open article and Reddit comments in default browser
+    - `⌘+L` — Show full post title in Alfred's Large Type window
+    - `⇧` or `⌘+Y` — Show Quick Look preview of comments page (or post. See [
 
-**Note:** OS X's "delete word" shortcut (`⌥+⌫`) is very handy for backing
-out of a subreddit.
+**Note:** OS X's "delete word" shortcut (`⌥+⌫`) is very handy for backing out of a subreddit.
 
 
 Description
 -----------
 
-A fairly rudimentary workflow to browse subreddits.
+A basic workflow to browse subreddits.
 
-The subreddit search, `r/<query>`, uses Reddit's API to search for subreddits
-that match `<query>`. 25 results are retrieved by default.
+The subreddit search, `r/<query>`, uses Reddit's API to search for subreddits that match `<query>`. 25 results are retrieved by default.
 
-The subreddit search can be a bit odd, which is due to the oddness of Reddit's
-search function.
+The subreddit search can be a bit odd, which is due to the legendary crapness of Reddit's search function.
 
-Search within a subreddit, `r/subreddit/<query>`, only filters the list of hot
-results. 50 results are retrieved by default.
+Search within a subreddit, `r/subreddit/<query>`, only filters the list of hot results. 50 results are retrieved by default.
+
+
+Configuration
+-------------
+
+There is one option, `QUICKLOOK_POST`, in the workflow's configuration sheet (in Alfred Preferences). Set `QUICKLOOK_POST` to `1` for the Quick Look preview to show the article instead of the Reddit comment page.
 
 
 Licensing, thanks etc.
@@ -57,6 +63,7 @@ I found the logo in a [Font Awesome issue][logo-source] on GitHub.
 
 It's heavily based on [Alfred-Workflow][alfred-workflow], also [MIT-licensed][mit].
 
+
 [reddit]: http://www.reddit.com
 [mit]: http://opensource.org/licenses/MIT
 [alfred-workflow]: http://www.deanishe.net/alfred-workflow/
@@ -64,3 +71,4 @@ It's heavily based on [Alfred-Workflow][alfred-workflow], also [MIT-licensed][mi
 [gh-releases]: https://github.com/deanishe/alfred-reddit/releases
 [packal-page]: http://www.packal.org/workflow/reddit
 [demo]: https://raw.githubusercontent.com/deanishe/alfred-reddit/master/demo.gif
+[v13]: https://github.com/deanishe/alfred-reddit/releases/tag/v1.3
--- src/reddit.py
+++ src/reddit.py
@@ -8,30 +8,40 @@
 # Created on 2014-12-29
 #
 
-"""reddit.py [options] [args]
+"""reddit.py [options] <query>
+
+Browse and search subreddits and hot posts.
 
 Usage:
     reddit.py <query>
-    reddit.py --openurl <postdata>
-    reddit.py --opencomments <postdata>
+    reddit.py [-c] [-p] [-s]
 
 Options:
-    -h, --help       Show this help text
-    --openurl        Open the URL of the post in default browser
-    --opencomments   Open the URL of Reddit post in default browser
+    -c, --comments        Open Reddit comments page in browser
+    -p, --post            Open post in browser
+    -s, --subreddit       Open subreddit in browser
+    -h, --help            Show this help text
 
 """
 
 from __future__ import print_function, unicode_literals, absolute_import
 
 from datetime import datetime
-import json
+from functools import partial
 import os
 import re
 import subprocess
 import sys
 
 from workflow import Workflow3, web, ICON_WARNING
+
+
+# dP     dP                   oo          dP       dP
+# 88     88                               88       88
+# 88    .8P .d8888b. 88d888b. dP .d8888b. 88d888b. 88 .d8888b. .d8888b.
+# 88    d8' 88'  `88 88'  `88 88 88'  `88 88'  `88 88 88ooood8 Y8ooooo.
+# 88  .d8P  88.  .88 88       88 88.  .88 88.  .88 88 88.  ...       88
+# 888888'   `88888P8 dP       dP `88888P8 88Y8888' dP `88888P' `88888P'
 
 # How many posts to retrieve
 POST_COUNT = 50
@@ -51,19 +61,44 @@ HELP_URL = 'https://github.com/deanishe/alfred-reddit'
 ICON_REDDIT = os.path.join(os.path.dirname(__file__), 'icon.png')
 ICON_UPDATE = os.path.join(os.path.dirname(__file__), 'update-available.png')
 
-POST_URL = 'https://www.reddit.com/r/{name}/hot.json'
+# JSON list of hot posts in subreddit
+HOT_POSTS_URL = 'https://www.reddit.com/r/{name}/hot.json'
 
+# JSON subreddit search
 SEARCH_URL = 'https://www.reddit.com/subreddits/search.json'
 
+# JSON list of popular subreddits
 POPULAR_URL = 'https://www.reddit.com/subreddits/popular.json'
+
+# HTML URL of subreddit
+SUBREDDIT_URL = 'https://www.reddit.com/r/{display_name}/'
+
+# HTML URL of post
+POST_URL = 'https://www.reddit.com{permalink}'
 
 USER_AGENT = 'Alfred-Reddit/{version} ({url})'
 
+# Populated on run
 log = None
 
 
+# dP     dP           dP
+# 88     88           88
+# 88aaaaa88a .d8888b. 88 88d888b. .d8888b. 88d888b. .d8888b.
+# 88     88  88ooood8 88 88'  `88 88ooood8 88'  `88 Y8ooooo.
+# 88     88  88.  ... 88 88.  .88 88.  ... 88             88
+# dP     dP  `88888P' dP 88Y888P' `88888P' dP       `88888P'
+#                        88
+#                        dP
+
+def open_url(url):
+    """Open URL in default browser."""
+    log.debug('Opening : %s', url)
+    subprocess.call(['open', url])
+
+
 def cache_key(name):
-    """Make filesystem-friendly cache key"""
+    """Make filesystem-friendly cache key."""
     key = name.lower()
     key = re.sub(r'[^a-z0-9-_\.]', '-', key)
     key = re.sub(r'-+', '-', key)
@@ -72,7 +107,7 @@ def cache_key(name):
 
 
 def relative_time(timestamp):
-    """Return human-readable, relative time"""
+    """Return human-readable, relative time."""
     now = datetime.utcnow()
     postdate = datetime.utcfromtimestamp(timestamp)
     delta = now - postdate
@@ -93,27 +128,37 @@ def relative_time(timestamp):
         return '1 sec ago'
 
 
-def handle_post(api_dict):
+#  888888ba                 dP       dP oo   dP       .d888888   888888ba  dP
+#  88    `8b                88       88      88      d8'    88   88    `8b 88
+# a88aaaa8P' .d8888b. .d888b88 .d888b88 dP d8888P    88aaaaa88a a88aaaa8P' 88
+#  88   `8b. 88ooood8 88'  `88 88'  `88 88   88      88     88   88        88
+#  88     88 88.  ... 88.  .88 88.  .88 88   88      88     88   88        88
+#  dP     dP `88888P' `88888P8 `88888P8 dP   dP      88     88   dP        dP
+
+def parse_post(api_dict):
     """Strip down API dict."""
-    data = api_dict.get('data', {})
-    post = {}
-    for key in ('title', 'url', 'author'):
-        post[key] = data[key]
-    post['timestamp'] = data['created_utc']
-    post['reltime'] = relative_time(data['created_utc'])
-    post['permalink'] = 'https://www.reddit.com{}'.format(data['permalink'])
+    d = api_dict.get('data', {})
+    post = {
+        'title': d['title'],
+        'post_url': d['url'],
+        'author': d['author'],
+        'timestamp': d['created_utc'],
+        'reltime': relative_time(d['created_utc']),
+        'comments_url': POST_URL.format(**d)
+    }
+    post['selfpost'] = post['post_url'] == post['comments_url']
     return post
 
 
-def handle_subreddit(api_dict):
+def parse_subreddit(api_dict):
     """Strip down API dict."""
-    data = api_dict.get('data', {})
-    sr = {}
-    sr['name'] = data['display_name']
-    sr['title'] = data['title']
-    sr['type'] = data['subreddit_type']
-
-    return sr
+    d = api_dict.get('data', {})
+    return {
+        'name': d['display_name'],
+        'title': d['title'],
+        'type': d['subreddit_type'],
+        'url': SUBREDDIT_URL.format(**d),
+    }
 
 
 def popular_subreddits(limit=SUBREDDIT_COUNT):
@@ -133,7 +178,7 @@ def popular_subreddits(limit=SUBREDDIT_COUNT):
 
     subreddits = r.json()['data']['children']
     # log.debug(pformat(subreddits))
-    subreddits = [handle_subreddit(d) for d in subreddits]
+    subreddits = [parse_subreddit(d) for d in subreddits]
     subreddits = [d for d in subreddits if d['type'] != 'private']
 
     for sr in subreddits:
@@ -159,7 +204,7 @@ def search_subreddits(query, limit=SUBREDDIT_COUNT):
 
     subreddits = r.json()['data']['children']
 
-    subreddits = [handle_subreddit(d) for d in subreddits]
+    subreddits = [parse_subreddit(d) for d in subreddits]
     # Only show public subreddits
     subreddits = [d for d in subreddits if d['type'] == 'public']
 
@@ -169,10 +214,10 @@ def search_subreddits(query, limit=SUBREDDIT_COUNT):
     return subreddits
 
 
-def subreddit(name, limit=POST_COUNT):
-    """Return list of hot posts on specified subreddit"""
+def hot_posts(name, limit=POST_COUNT):
+    """Return list of hot posts on specified subreddit."""
     log.debug('Fetching hot posts in r/%s ...', name)
-    url = POST_URL.format(name=name)
+    url = HOT_POSTS_URL.format(name=name)
     headers = {'user-agent': USER_AGENT.format(version=wf.version,
                                                url=wf.help_url)}
     params = {'limit': limit}
@@ -190,7 +235,7 @@ def subreddit(name, limit=POST_COUNT):
     r.raise_for_status()
 
     data = r.json()
-    posts = [handle_post(d) for d in data['data']['children']]
+    posts = [parse_post(d) for d in data['data']['children']]
 
     return posts
 
@@ -205,6 +250,146 @@ def subreddit_search_key(sr):
     return '{} {}'.format(sr['name'], sr['title'])
 
 
+# dP   dP   dP                   dP       .8888b dP
+# 88   88   88                   88       88   " 88
+# 88  .8P  .8P .d8888b. 88d888b. 88  .dP  88aaa  88 .d8888b. dP  dP  dP
+# 88  d8'  d8' 88'  `88 88'  `88 88888"   88     88 88'  `88 88  88  88
+# 88.d8P8.d8P  88.  .88 88       88  `8b. 88     88 88.  .88 88.88b.88'
+# 8888' Y88'   `88888P' dP       dP   `YP dP     dP `88888P' 8888P Y8P
+
+def show_popular():
+    """List popular subreddits."""
+    subreddits = wf.cached_data('--popular', popular_subreddits,
+                                max_age=CACHE_MAX_AGE)
+
+    for sr in subreddits:
+        url = sr['url']
+        it = wf.add_item(sr['name'],
+                         sr['title'],
+                         autocomplete='{}/'.format(sr['name']),
+                         quicklookurl=url,
+                         icon=ICON_REDDIT)
+
+        it.setvar('subreddit_url', url)
+        it.add_modifier('cmd',
+                        'View "r/{}" in browser'.format(sr['name']),
+                        valid=True).setvar('argv', '-s')
+
+    wf.send_feedback()
+    return 0
+
+
+def show_search(name):
+    """List subreddits matching `name`."""
+    key = '--search-{}'.format(cache_key(name))
+
+    subreddits = wf.cached_data(key,
+                                partial(search_subreddits, name),
+                                max_age=CACHE_MAX_AGE)
+
+    # Filter results because Reddit's search is super-crappy
+    subreddits = wf.filter(name, subreddits,
+                           key=subreddit_search_key,
+                           min_score=30)
+
+    if not subreddits:
+
+        wf.add_item('No matching subreddits found',
+                    'Try a different query',
+                    icon=ICON_WARNING)
+
+    else:  # List all matching subreddits
+
+        for sr in subreddits:
+
+            log.debug(repr(sr))
+
+            url = sr['url']
+            it = wf.add_item(sr['name'],
+                             sr['title'],
+                             autocomplete='{}/'.format(sr['name']),
+                             arg=url,
+                             uid=sr['name'],
+                             quicklookurl=url,
+                             valid=True,
+                             icon=ICON_REDDIT)
+
+            it.setvar('subreddit_url', url)
+            it.setvar('argv', '-s')
+
+    wf.send_feedback()
+    return 0
+
+
+def show_posts(name, query):
+    """List posts within subreddit `name`."""
+    # Whether Quick Look shows post or comments
+    qlpost = os.getenv('QUICKLOOK_POST') == "1"
+
+    # Filesystem-friendly key
+    key = cache_key(name)
+
+    log.debug('Viewing r/%s ...', name)
+
+    posts = wf.cached_data(key,
+                           partial(hot_posts, name),
+                           max_age=CACHE_MAX_AGE)
+
+    if posts is None:  # Non-existent subreddit
+        wf.add_item('r/{} does not exist'.format(name),
+                    'Try a different name',
+                    icon=ICON_WARNING)
+
+        wf.send_feedback()
+        return 0
+
+    if query:
+        posts = wf.filter(query, posts, key=post_search_key, min_score=30)
+
+    if not posts:
+        wf.add_item('No matching results found',
+                    'Try a different subreddit and/or query',
+                    icon=ICON_WARNING)
+
+    for post in posts:
+        subtitle = 'Posted {} by {} // {}'.format(post['reltime'],
+                                                  post['author'],
+                                                  post['post_url'])
+
+        if qlpost:
+            qlurl = post['post_url']
+        else:
+            qlurl = post['comments_url']
+
+        it = wf.add_item(post['title'],
+                         subtitle,
+                         arg=post['post_url'],
+                         largetext=post['title'],
+                         valid=True,
+                         quicklookurl=qlurl,
+                         icon=ICON_REDDIT)
+
+        it.setvar('post_url', post['post_url'])
+        it.setvar('comments_url', post['comments_url'])
+        it.setvar('argv', '-p')
+
+        csub = 'View comments on Reddit'
+        cargv = '-c'
+        asub = 'View both article and comments in browser'
+        aargv = '-c -p'
+
+        if post['selfpost']:
+            csub = asub = '[Self post] View on Reddit'
+            aargv = '-c'
+
+        it.add_modifier('cmd', csub, valid=True).setvar('argv', cargv)
+        it.add_modifier('alt', asub, valid=True).setvar('argv', aargv)
+
+    wf.send_feedback()
+
+    log.debug('%d hot posts in subreddit `%s`', posts, name)
+
+
 def main(wf):
     """Run workflow."""
     from docopt import docopt
@@ -215,19 +400,21 @@ def main(wf):
     # Run Script actions
     # ------------------------------------------------------------------
 
-    if args.get('<postdata>'):
+    done = False
+    if args.get('--post'):
+        open_url(os.getenv('post_url'))
+        done = True
 
-        d = json.loads(args.get('<postdata>'))
+    if args.get('--comments'):
+        open_url(os.getenv('comments_url'))
+        done = True
 
-        if args.get('--openurl'):
-            url = d['url']
-        elif args.get('--opencomments'):
-            url = d['permalink']
+    if args.get('--subreddit'):
+        open_url(os.getenv('subreddit_url'))
+        done = True
 
-        log.debug('Opening : %s', url)
-        subprocess.call(['open', url])
-
-        return 0
+    if done:
+        return
 
     ####################################################################
     # Script Filter
@@ -235,7 +422,6 @@ def main(wf):
 
     # Updates
     # ------------------------------------------------------------------
-
     if wf.update_available:
         wf.add_item('A newer version is available',
                     '↩ to install update',
@@ -243,26 +429,12 @@ def main(wf):
                     icon=ICON_UPDATE)
 
     query = args.get('<query>')
-
     log.debug('query : %r', query)
 
     # Show popular subreddits
     # ------------------------------------------------------------------
-
     if query == '':
-
-        subreddits = wf.cached_data('--popular', popular_subreddits,
-                                    max_age=CACHE_MAX_AGE)
-
-        for sr in subreddits:
-
-            wf.add_item(sr['name'],
-                        sr['title'],
-                        autocomplete='{}/'.format(sr['name']),
-                        icon=ICON_REDDIT)
-
-        wf.send_feedback()
-        return 0
+        return show_popular()
 
     # Parse query
     # ------------------------------------------------------------------
@@ -285,102 +457,12 @@ def main(wf):
 
     # Search for matching subreddit
     # ------------------------------------------------------------------
-
     if not slash:
-
-        key = '--search-{}'.format(cache_key(name))
-
-        subreddits = wf.cached_data(key, lambda: search_subreddits(name),
-                                    max_age=CACHE_MAX_AGE)
-
-        # Filter results because Reddit's search is super-crappy
-        subreddits = wf.filter(name, subreddits,
-                               key=subreddit_search_key,
-                               min_score=30)
-
-        if not subreddits:
-
-            wf.add_item('No matching subreddits found',
-                        'Try a different query',
-                        icon=ICON_WARNING)
-
-        else:  # List all matching subreddits
-
-            for sr in subreddits:
-
-                log.debug(repr(sr))
-
-                # Encode arg to send to Run Script
-                url = 'https://www.reddit.com/r/{}'.format(sr['name'])
-                arg = json.dumps({
-                    'name': sr['name'],
-                    'title': sr['title'],
-                    'url': url})
-
-                wf.add_item(sr['name'],
-                            sr['title'],
-                            autocomplete='{}/'.format(sr['name']),
-                            arg=arg,
-                            uid=sr['name'],
-                            valid=True,
-                            icon=ICON_REDDIT)
-
-        wf.send_feedback()
-        return 0
+        return show_search(name)
 
     # Browse/search within subreddit
     # ------------------------------------------------------------------
-
-    # Filesystem-friendly key
-    key = cache_key(name)
-
-    log.debug('Viewing r/%s ...', name)
-
-    posts = wf.cached_data(key, lambda: subreddit(name), max_age=CACHE_MAX_AGE)
-
-    if posts is None:  # Non-existent subreddit
-        wf.add_item('r/{} does not exist'.format(name),
-                    'Try a different name',
-                    icon=ICON_WARNING)
-
-        wf.send_feedback()
-        return 0
-
-    if query:
-        posts = wf.filter(query, posts, key=post_search_key, min_score=30)
-
-    if not posts:
-        wf.add_item('No matching results found',
-                    'Try a different subreddit and/or query',
-                    icon=ICON_WARNING)
-
-    for post in posts:
-        subtitle = 'Posted {} by {} // {}'.format(post['reltime'],
-                                                  post['author'],
-                                                  post['url'])
-
-        # Encode arg to send to Run Script
-        arg = json.dumps({'url': post['url'], 'permalink': post['permalink']})
-
-        modifiers = {}
-
-        if post['url'] != post['permalink']:  # Not a Self post
-            modifiers['cmd'] = 'View Comments on Reddit'
-        else:
-            modifiers['cmd'] = '[Self post] View on Reddit'
-
-        wf.add_item(post['title'],
-                    subtitle,
-                    modifier_subtitles=modifiers,
-                    arg=arg,
-                    largetext=post['title'],
-                    valid=True,
-                    quicklookurl=post['permalink'],
-                    icon=ICON_REDDIT)
-
-    wf.send_feedback()
-
-    log.debug('%d hot posts in subreddit `%s`', posts, name)
+    return show_posts(name, query)
 
 
 if __name__ == '__main__':
